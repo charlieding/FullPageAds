a3cb00ef3f7eeb26f9c1df0d0631f281dad13b05
Charles Ding <chdin@microsoft.com>
2017-03-17 16:02:58 -0700

    Testing First Continuous Deployment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var port = process.env.port || 1337
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send('Hello World! Author: Charles Ding - Deployment Options Set!');
 });
 
 var server = app.listen(port, function () {
